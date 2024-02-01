71a5988235eb979a5316682799019d951060711b
Luis Avila <zluissigloxx@hotmail.com>
2024-02-01 08:11:15 -0500

    cambios ip adulto mayor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true
-  , API_URL: "http://192.168.100.90/adultomayorapi"
+  production: true,
+  API_URL: 'http://138.186.20.99:3563/adultomayorapi',
   //, API_URL: "http://190.131.195.10:3563/adultomayorapi"
-  , COOKIE_SESSION: 'USER'
+  COOKIE_SESSION: 'USER',
 };
